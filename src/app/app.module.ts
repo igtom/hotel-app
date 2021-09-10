ff49c5265cdf8098b323ce7ca028749d637b7139
igtom <igtom.martin@gmail.com>
2021-09-10 21:41:15 +0200

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookingsComponent } from './components/bookings/bookings.component';
+import { CreateBookingComponent } from './components/create-booking/create-booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookingsComponent
+    BookingsComponent,
+    CreateBookingComponent
   ],
   imports: [
     BrowserModule,
