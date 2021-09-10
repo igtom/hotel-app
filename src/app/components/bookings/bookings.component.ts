b40407e7ef1a0e4ea45e20920d6a95559b73bf27
igtom <igtom.martin@gmail.com>
2021-09-10 21:18:44 +0200

    booking

--- src/app/components/bookings/bookings.component.ts
+++ src/app/components/bookings/bookings.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Booking } from 'src/app/models/booking';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.component.html',
-  styleUrls: ['./bookings.component.scss']
+  styleUrls: ['./bookings.component.scss'],
 })
 export class BookingsComponent implements OnInit {
+  booking: Booking = {
+    id: 1,
+    name: 'Tom Martin',
+    roomNumber: 187,
+    startDate: new Date(),
+    endDate: new Date('2021-09-21'),
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
